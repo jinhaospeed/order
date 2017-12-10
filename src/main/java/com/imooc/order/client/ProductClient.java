5c5955c0f170480a0dec02615dc348583f2c090c
廖师兄 <null@163.com>
2017-12-10 23:49:28 +0800

    商品扣库存接口

--- src/main/java/com/imooc/order/client/ProductClient.java
+++ src/main/java/com/imooc/order/client/ProductClient.java
@@ -1,6 +1,7 @@
 package com.imooc.order.client;
 
 import com.imooc.order.dataobject.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,4 +22,6 @@ public interface ProductClient {
     @PostMapping("/product/listForOrder")
     List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
 
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<CartDTO> cartDTOList);
 }
