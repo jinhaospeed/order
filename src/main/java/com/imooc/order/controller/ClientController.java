893a4d4a7d53d6b690a4aad7203ea5740b0e51c5
廖师兄 <null@163.com>
2018-01-19 22:22:26 +0800

    负载均衡规则设置

--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.imooc.order.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ClientController {
 
-//    @Autowired
-//    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -27,13 +29,13 @@ public class ClientController {
 //        String response = restTemplate.getForObject("http://localhost:8080/msg", String.class);
 
         //2. 第二种方式(利用loadBalancerClient通过应用名获取url, 然后再使用restTemplate)
-//        RestTemplate restTemplate = new RestTemplate();
-//        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
-//        String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()) + "/msg";
-//        String response = restTemplate.getForObject(url, String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
+        String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()) + "/msg";
+        String response = restTemplate.getForObject(url, String.class);
 
         //3. 第三种方式(利用@LoadBalanced, 可在restTemplate里使用应用名字)
-        String response = restTemplate.getForObject("http://PRODUCT/msg", String.class);
+//        String response = restTemplate.getForObject("http://PRODUCT/msg", String.class);
 
         log.info("response={}", response);
         return response;
