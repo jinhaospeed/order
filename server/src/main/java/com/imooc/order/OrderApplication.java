899fa433118eb15c222e66a0ee34ae7f4e2155a9
廖师兄 <null@163.com>
2018-03-18 21:39:25 +0800

    hystrix降级和熔断

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -1,13 +1,14 @@
 package com.imooc.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.imooc.product.client")
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
 public class OrderApplication {
 
 	public static void main(String[] args) {
