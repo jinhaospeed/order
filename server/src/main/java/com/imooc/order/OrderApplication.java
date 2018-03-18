4515b7c64767d9a208c6ca292437a0eae4e43c82
廖师兄 <null@163.com>
2018-03-18 22:06:13 +0800

    hystrix-dashboard的使用

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -3,12 +3,14 @@ package com.imooc.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 //@SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableCircuitBreaker
 @SpringCloudApplication
+@EnableHystrixDashboard
 public class OrderApplication {
 
 	public static void main(String[] args) {
